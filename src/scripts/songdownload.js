7a3663cf58f3f66f7b256ed4096770a5419b7243
brelee2222 <brelee1842@outlook.com>
2024-06-26 16:45:34 -0700

    Changes (and unsuccessful work)

--- src/scripts/songdownload.js
+++ src/scripts/songdownload.js
@@ -1,1 +1,16 @@
-setTimeout(() => console.log(document.getElementsByClassName("vjs-tech")[0].src), 1000);
+window.addEventListener("load", async e => {
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    console.log("The download script is running")
+    if(!document.URL.includes("www.smule.com/recording/")) 
+        return;
+    let moreActionsButton;
+    for(const element of document.getElementsByTagName("button")) if(element.ariaLabel == "More actions") {
+        moreActionsButton = element;
+        break;
+    }
+    console.log(moreActionsButton);
+    if(!moreActionsButton) 
+        throw new Error("'more actions' button not found");
+});
