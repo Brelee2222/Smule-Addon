a54dfbe96a341c38f8522db6baa41245c233425d
brelee2222 <brelee1842@outlook.com>
2024-06-26 17:49:19 -0700

    added download button

--- src/scripts/songdownload.js
+++ src/scripts/songdownload.js
@@ -1,16 +1,47 @@
-window.addEventListener("load", async e => {
+(async () => {
+    while(true) {
+    // console.log("The download script is running")
+    // console.log(window.location.href);
+        if(!window.location.href.includes("recording/")) {
+            await new Promise(resolve => setTimeout(resolve, 10));
+            continue;
+        }
+        let moreActionsButton;
+        for(const element of document.getElementsByTagName("button")) if(element.ariaLabel == "More actions") {
+            moreActionsButton = element;
+            break;
+        }
+        // console.log("The download script is runningasga")
+        if(!moreActionsButton) {
+            console.log("'more actions' button not found");
+            await new Promise(resolve => setTimeout(resolve, 500));
+            continue;
+        }
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+        if(moreActionsButton.injected) {
+            await new Promise(resolve => setTimeout(resolve, 500));
+            continue;
+        }
 
-    console.log("The download script is running")
-    if(!document.URL.includes("www.smule.com/recording/")) 
-        return;
-    let moreActionsButton;
-    for(const element of document.getElementsByTagName("button")) if(element.ariaLabel == "More actions") {
-        moreActionsButton = element;
-        break;
+        moreActionsButton.injected = true;
+        moreActionsButton.addEventListener("click", async () => {
+            await new Promise(resolve => setTimeout(resolve, 10));
+
+            for(const element of moreActionsButton.parentElement.children) if(element.tagName === "NAV") {
+                const navList = element.children[0];
+
+                const downloadButton = navList.appendChild(navList.children[1].cloneNode(2));
+
+                downloadButton.querySelector("span span").innerText = "Download";
+
+                const aElement = downloadButton.querySelector("a");
+
+                aElement.href = document.getElementsByClassName("vjs-tech")[0].src;
+
+                aElement.download = true;
+
+                break;
+            }
+        });
     }
-    console.log(moreActionsButton);
-    if(!moreActionsButton) 
-        throw new Error("'more actions' button not found");
-});
+})();
